0c7c364a8dd58d7193427e066f178bff1aa6d2e7
Auridebson Lima <auridebson@vutus.com>
2023-12-08 11:30:48 -0300

    Update

--- main4.py
+++ main4.py
@@ -1,14 +1,29 @@
 from func import *
 
-numeros = [1,2,3,4,5,6,7,8,90]
+ln(30)
+print("Use esse programa pra fazer uma saudação equivalente ao turno")
+ln(30)
 
-def soma(n1,n2):
-    resultado = n1 + n2
+numeros = [1,2,3,4,5,6,7,8,90]
+resultado = 0
+
+def saudacao(hora):
+    if (hora >= 0) and (hora < 6):
+        resultado = f"Bom dia na madrugada"
+    elif (hora >= 6) and (hora < 12):
+        resultado = f"Bom dia!"
+    elif (hora >= 12) and (hora < 18):
+        resultado = f"Boa tarde"
+    elif (hora >= 18) and (hora <= 23):
+        resultado = f"Boa noite"
+    else:
+        resultado = f"Digite um número válido e inteiro para a hora"
+
     return resultado
 
-acumulado = 0
+entradaHora = int(input("Digite a hora da saudação: "))
 
-for num in numeros:
-    acumulado += num
+ln(30)
+print(saudacao(entradaHora))
+ln(30)
 
-print(acumulado)
