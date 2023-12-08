884806432ab64b256a3d9057e7167d4bdcc0b51c
Auridebson Lima <auridebson@vutus.com>
2023-12-08 10:05:45 -0300

    Update

--- main4.py
+++ main4.py
@@ -1,9 +1,14 @@
 from func import *
 
+nomes = ["auridebson", "luciana", "levy", "guilherme"]
+
 def saudacao(nome1, nome2):
+    if  nome1 in nomes:
+        print("Poxa! Você está olhando para um cara lindo!")
+        print("------")
     print(f'Olá, você está olhando para {nome1} mas quem está ao seu lado é {nome2}')
 
 
 ln(30)
-saudacao("João","Maria")
+saudacao("luciana","Maria")
 ln(30)
