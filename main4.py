b0f1a713edabd898b817c68688ff35cde266a8bf
Auridebson Lima <auridebson@vutus.com>
2023-12-08 10:32:34 -0300

    Update

--- main4.py
+++ main4.py
@@ -1,14 +1,14 @@
 from func import *
 
-nomes = ["auridebson", "luciana", "levy", "guilherme"]
+numeros = [1,2,3,4,5,6,7,8,90]
 
-def saudacao(nome1, nome2):
-    if  nome1 in nomes:
-        print("Poxa! Você está olhando para um cara lindo!")
-        print("------")
-    print(f'Olá, você está olhando para {nome1} mas quem está ao seu lado é {nome2}')
+def soma(n1,n2):
+    resultado = n1 + n2
+    return resultado
 
+acumulado = 0
 
-ln(30)
-saudacao("luciana","Maria")
-ln(30)
+for num in numeros:
+    acumulado += num
+
+print(acumulado)
