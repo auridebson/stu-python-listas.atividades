d3e818b48aa2d6cdb1531cc418d37e4c8020b7da
Auridebson Lima <auridebson@vutus.com>
2023-12-08 09:38:57 -0300

    Update

---
+++ main3.py
@@ -0,0 +1,27 @@
+from func import *
+
+
+# PEDIR AO USUÁRIO: NOME, IDADE, ALTURA, SALÁRIO
+# GUARDE AS INFORMAÇÕES EM UM DICIONÁRIO
+# CHECAR:
+# - SE MAIOR QUE 18 MOSTRE MENSAGEM NA TELA
+# - SE SALÁRIO < QUE 1500 MOSTRE MENSAGEM NA TELA
+# - SE SALÁRIO < 1500
+# - SE SALÁRIO FOR < 3000 E > 1500
+# - SE SALÁRIO FOR < 5000 E > 3000
+# - SE SALÁRIO FOR > 5000
+
+bd = {
+    "id": 1,
+    "nome": "auridebson",
+    "idade": 46,
+    "salario": 100000,
+    
+
+}
+
+
+ln(30)
+print("Inclusão de dados de usuário no sistema")
+ln(30)
+
